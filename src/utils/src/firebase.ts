428916012be147feb2029c20021b13db6fc0334d
castanhasintegralnuts-glitch <castanhasintegralnuts@gmail.com>
2026-08-12 22:01:20 -0300

    Create firebase.ts

---
+++ src/utils/src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTVlkwDSuMj2S6F6T9pkfWw-PNsLo8be8",
+  authDomain: "epr-ind-nuts.firebaseapp.com",
+  projectId: "epr-ind-nuts",
+  storageBucket: "epr-ind-nuts.firebasestorage.app",
+  messagingSenderId: "139565874940",
+  appId: "1:139565874940:web:f119c8e9a40e1fc1b26179"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
